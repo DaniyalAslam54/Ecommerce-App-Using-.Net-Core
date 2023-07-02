145bf11e42e5a9de7d0189996a01919547ecc45d
Daniyal Aslam <daniyalaslam54@gmail.com>
2023-07-02 20:14:18 +0500

    category crud completed

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BulkyWeb.Models
+{
+    [Table("Categories")]
+    public class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+         
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+        [Required]
+        [DisplayName("Display Order")]
+
+        [Range(1, 100)]
+        //[Range(1, 100, ErrorMessage  = "")]
+        public int DisplayOrder { get; set; }
+
+
+        
+    }
+}
